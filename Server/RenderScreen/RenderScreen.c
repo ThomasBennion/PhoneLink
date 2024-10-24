2dcfc38de8b18a56f06b03d05b0b06a1b99f1efe
Radula <Radula@bessie.lan>
2024-10-24 16:17:56 +1000

    Added openGL texture drawing functionality

--- Server/RenderScreen/RenderScreen.c
+++ Server/RenderScreen/RenderScreen.c
@@ -8,6 +8,73 @@ void Framebuffer_Size_Callback(GLFWwindow* window, int width, int height) {
     glViewport(0,0, width, height);
 }
 
+char* loadShader(const char* filePath) {
+    FILE* file = fopen(filePath, "rb");
+    if (!file) {
+        fprintf(stderr, "Unable to open file '%s'\n", filePath);
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char* source = (char*)malloc(length + 1);
+    fread(source, 1, length, file);
+    source[length] = '\0';
+
+    fclose(file);
+    return source;
+}
+
+GLuint compileShader(const char* filePath, GLenum shaderType) {
+    char* shaderSource = loadShader(filePath);
+    
+    if (!shaderSource) {
+        return 0;
+    }
+
+    GLuint shader = glCreateShader(shaderType);
+    
+    glShaderSource(shader, 1, (const GLchar* const*)&shaderSource, NULL);
+    glCompileShader(shader);
+
+    GLint success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char infoLog[512];
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        fprintf(stderr, "Shader compilation failed: '%s'\n", infoLog);
+    }
+
+    free(shaderSource);
+    
+    return shader;
+}
+
+GLuint createShaderProgram(const char* vertexPath, const char* fragmentPath) {
+    GLuint vertexShader = compileShader(vertexPath, GL_VERTEX_SHADER);
+    GLuint fragmentShader = compileShader(fragmentPath, GL_FRAGMENT_SHADER);
+    
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    GLint success;
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        char infoLog[512];
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        fprintf(stderr, "Program linking failed '%s'\n", infoLog);
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
 PyObject* Render_Screen(PyObject* self, PyObject* args) {
     
     PyObject* Fail = PyLong_FromLong(-1);
@@ -43,6 +110,63 @@ PyObject* Render_Screen(PyObject* self, PyObject* args) {
     glfwMakeContextCurrent(window);
     //glScalef(1, -1, 1);
     //int counter = 0;
+    const char* vertexPath = "RenderScreen/vertex_shader.glsl";
+    const char* fragmentPath = "RenderScreen/fragment_shader.glsl";
+    
+    glewInit();
+
+    GLuint shaderProgram = createShaderProgram(vertexPath, fragmentPath);
+        
+    unsigned int VAO;
+    unsigned int VBO;
+    unsigned int EBO;
+    
+    // Vertex and Index data
+    float vertices[] = {
+        // Positions    // Texture Coords
+        1.0f, -1.0f,     width, height,  // Bottom-left
+        1.0f, 1.0f,    width, 0.0f,  // Bottom-right
+        -1.0f, 1.0f,   0.0f, 0.0f,  // Top-right
+        -1.0f, -1.0f,    0.0f, height   // Top-left
+    };
+    
+    unsigned int indices[] = {
+        0, 1, 2,  // First triangle
+        2, 3, 0   // Second triangle
+    };
+
+    // Generate VAO, VBO, and EBO
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    // Bind VAO
+    glBindVertexArray(VAO);
+
+    // Bind and fill VBO with vertex data
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Bind and fill EBO with index data
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Set up vertex position attribute
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Set up texture coordinate attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // Unbind VAO (optional)
+    //glBindVertexArray(0);
+    
+    unsigned int texID;
+    glGenTextures (1, &texID);
+    glBindTexture (GL_TEXTURE_RECTANGLE, texID);
+
+    glTexImage2D (GL_TEXTURE_RECTANGLE_EXT, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
     /* Loop until the user closes the window */
     Py_BEGIN_ALLOW_THREADS
     while (!glfwWindowShouldClose(window))
@@ -50,27 +174,21 @@ PyObject* Render_Screen(PyObject* self, PyObject* args) {
         //fprintf(stdout, data);
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        glRasterPos2f(-1,1);
-        glPixelZoom(1, -1);
+        //glRasterPos2f(-1,1);
+        //glPixelZoom(1, -1);
         
-        /*
-        unsigned int* texID;
-        glGenTextures (1, texID);
-        glBindTextures (GL_TEXTURE_RECTANGLE_EXT, 1, texID);
-        glTexImage2D (GL_TEXTURE_RECTANGLE_EXT, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glBegin (GL_QUADS);
-        glTexCoord2f (0, 0);
-        glVertex2f (0, 0);
-        glTexCoord2f (width, 0);
-        glVertex2f (windowWidth, 0);
-        glTexCoord2f (width, height);
-        glVertex2f (windowWidth, windowHeight);
-        glTexCoord2f (0, height);
-        glVertex2f (0, windowHeight);
-        glEnd();
-        */
-
-        glDrawPixels(width, height, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glUseProgram(shaderProgram);
+        
+        glBindVertexArray(VAO);
+        
+        // Update the texture with new data every frame
+        glBindTexture(GL_TEXTURE_RECTANGLE, texID);
+        glTexSubImage2D(GL_TEXTURE_RECTANGLE, 0, 0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        
+        //glTexImage2D (GL_TEXTURE_RECTANGLE_EXT, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glDrawPixels(width, height, GL_RGBA, GL_UNSIGNED_BYTE, data);
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
